e55b203fe1623100abac0c1de250a14f7326d814
Youngtee230 <tlhomphomatona42@gmail.com>
2025-04-20 21:01:20 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=R1gBVI4Z#EdbBAGkJFf5zrxUFhB7ev0FVe3CXrU799KL3fKoKnoY",
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
